ed02fffa3c75d39e3d48f82e085852e3750ef25d
Chuck Cox <chuckc@us.ibm.com>
2023-02-16 13:51:46 -0600

    fix: var values

--- terraform/simple-openshift/main.tf
+++ terraform/simple-openshift/main.tf
@@ -28,8 +28,8 @@ resource "ibm_container_cluster" "cluster" {
   machine_type      = var.machine_type
   hardware          = var.hardware
   kube_version      = var.kube_version
-  public_vlan_id    = "vlan"
-  private_vlan_id   = "vlan"
+  public_vlan_id    = var.public_vlan_num
+  private_vlan_id   = var.private_vlan_num
   resource_group_id = data.ibm_resource_group.group.id
 }
 
