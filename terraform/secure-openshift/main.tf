afbe7a4f6a10b19ef317b9c91127624568d50811
Chuck Cox <chuckc@us.ibm.com>
2023-02-21 15:03:28 -0600

    chore: update var names

--- terraform/secure-openshift/main.tf
+++ terraform/secure-openshift/main.tf
@@ -28,8 +28,8 @@ resource "ibm_container_cluster" "cluster" {
   machine_type      = var.machine_type
   hardware          = var.hardware
   kube_version      = var.kube_version
-  public_vlan_id    = var.public_vlan_num
-  private_vlan_id   = var.private_vlan_num
+  public_vlan_id    = var.public_vlan_id
+  private_vlan_id   = var.private_vlan_id
   resource_group_id = data.ibm_resource_group.group.id
 }
 
